95dd63627650e5e9c48102eeb8e127695c608470
Jiffy <1796021400@qq.com>
2021-03-26 18:52:39 +0800

    数据注入

--- src/creator.js
+++ src/creator.js
@@ -1,22 +1,35 @@
 const chalk = require("chalk");
+const fs = require("fs-extra");
 const inquirer = require("inquirer");
+const memFs = require("mem-fs");
+const memFsEditor = require("mem-fs-editor");
+const path = require("path");
 
 class Creator {
   constructor() {
-    // 存储命令行获取的数据，作为demo这里只要这两个；
+    // 创建内存store
+    const store = memFs.create();
+    this.fs = memFsEditor.create(store);
+    // 存储命令行获取的数据，
     this.options = {
+      type: "",
       id: "",
       description: "",
       author: "",
-      name: "",
+      name: ""
     };
+
+    // 当前根目录
+    this.rootPath = path.resolve(__dirname, "../");
+    // 模板目录
+    this.tplDirpath = path.join(this.rootPath, "template");
   }
   // 初始化；
   // ...
   init() {
     console.log(chalk.green("my cli 开始"));
     console.log();
-    this.ask().then((answers) => {
+    this.ask().then(answers => {
       this.options = Object.assign({}, this.options, answers);
       this.write();
     });
@@ -28,49 +41,127 @@ class Creator {
     // 问题
     const questions = [
       {
+        type: "rawlist",
+        name: "type",
+        message: "你要画甚么盒子",
+        choices: ["管式盒", "文件夹/封套", "纸箱", "其他"]
+      },
+      {
         type: "input",
         name: "id",
-        message: "请输入盒子编号",
-        validate: function (value) {
+        message: "盒子的编号呢",
+        validate: function(value) {
           var pass = value.match(/^\d{6}$/);
           if (pass) {
             return true;
           }
           return "请输入正确的编号";
-        },
+        }
       },
       {
         type: "input",
         name: "description",
-        message: "请输入盒子中文名称",
+        message: "我想盒子应该有中文名称"
       },
       {
         type: "input",
         name: "author",
-        message: "请输入你的名字",
-        validate: function (input) {
+        message: "不得不留下你的名字",
+        validate: function(input) {
           if (!input) {
             return "请输入你的名字";
           }
           return true;
-        },
+        }
       },
       {
         type: "input",
         name: "name",
-        message: "请输入项目名称",
+        message: "这个项目叫甚么好呢",
         validate(input) {
           if (!input) {
             return "请输入项目名称!";
           }
 
-          //   if (fs.existsSync(input)) {
-          //     return "项目名已重复!";
-          //   }
+          if (fs.existsSync(input)) {
+            return "项目名已重复!";
+          }
 
           return true;
-        },
+        }
       },
+      // 制作尺寸
+      {
+        type: "input",
+        name: "KSLid",
+        message: "How many of the IIL with L?",
+        validate: function(value) {
+          var valid = !isNaN(parseFloat(value));
+          return valid || "Please enter a number";
+        },
+        filter: Number
+      },
+      {
+        type: "input",
+        name: "KSWid",
+        message: "How many of the IIL with W?",
+        validate: function(value) {
+          var valid = !isNaN(parseFloat(value));
+          return valid || "Please enter a number";
+        },
+        filter: Number
+      },
+      {
+        type: "input",
+        name: "KSHid",
+        message: "How many of the IIL with D?",
+        validate: function(value) {
+          var valid = !isNaN(parseFloat(value));
+          return valid || "Please enter a number";
+        },
+        filter: Number
+      },
+      {
+        type: "input",
+        name: "KSHof",
+        message: "How many of the OF with D?",
+        validate: function(value) {
+          var valid = !isNaN(parseFloat(value));
+          return valid || "Please enter a number";
+        },
+        filter: Number
+      },
+      // 外尺寸
+      {
+        type: "input",
+        name: "OSLd",
+        message: "How many of the CCAL with LW?",
+        validate: function(value) {
+          var valid = !isNaN(parseFloat(value));
+          return valid || "Please enter a number";
+        },
+        filter: Number
+      },
+      {
+        type: "input",
+        name: "OSWd",
+        message: "How many of the CCAL with WW?",
+        validate: function(value) {
+          var valid = !isNaN(parseFloat(value));
+          return valid || "Please enter a number";
+        },
+        filter: Number
+      },
+      {
+        type: "input",
+        name: "OSHd",
+        message: "How many of the CCAL with DW?",
+        validate: function(value) {
+          var valid = !isNaN(parseFloat(value));
+          return valid || "Please enter a number";
+        },
+        filter: Number
+      }
     ];
 
     // 返回promise
@@ -78,7 +169,28 @@ class Creator {
   }
 
   // 拷贝&写数据；
-  write() {}
+  write() {
+    console.log(chalk.green("my cli 构建开始"));
+    const tplBuilder = require("../template/index.js");
+    tplBuilder(this, this.options, () => {
+      console.log(chalk.green("my cli 构建完成"));
+      console.log();
+      console.log(chalk.grey(`开始项目:  npm run dev`));
+      // console.log(chalk.grey(`开始项目:  cd ${this.options.name} && npm install`));
+    });
+  }
+
+  getTplPath(file) {
+    return path.join(this.tplDirpath, file);
+  }
+  copyTpl(file, to, data = {}) {
+    const tplPath = this.getTplPath(file);
+    this.fs.copyTpl(tplPath, to, data);
+  }
+  copy(file, to) {
+    const tplPath = this.getTplPath(file);
+    this.fs.copy(tplPath, to);
+  }
 }
 
 module.exports = Creator;
